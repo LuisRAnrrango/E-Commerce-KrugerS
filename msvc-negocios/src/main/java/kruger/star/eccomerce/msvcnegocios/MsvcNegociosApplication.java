d8a71c884bd9412f6678cd1bc05ac111fdbee080
LuisRAnrrango <35409654+LuisRAnrrango@users.noreply.github.com>
2023-02-01 20:02:36 -0500

    Configuracion de Eureka en cada uno de los microservicios con su respectivo Client

--- msvc-negocios/src/main/java/kruger/star/eccomerce/msvcnegocios/MsvcNegociosApplication.java
+++ msvc-negocios/src/main/java/kruger/star/eccomerce/msvcnegocios/MsvcNegociosApplication.java
@@ -2,8 +2,10 @@ package kruger.star.eccomerce.msvcnegocios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsvcNegociosApplication {
 
 	public static void main(String[] args) {
