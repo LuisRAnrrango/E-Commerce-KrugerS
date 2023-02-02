e26bd4dc30180e14bd054919eca542d6c08abcaf
LuisRAnrrango <35409654+LuisRAnrrango@users.noreply.github.com>
2023-02-02 14:26:39 -0500

    Errores corregidos de Funcionamiento de Eureka y  posible solución a ejecución de Pedidos

--- msvc-estadopedidos/src/main/java/kruger/star/eccomerce/msvcestadopedidos/MsvcEstadopedidosApplication.java
+++ msvc-estadopedidos/src/main/java/kruger/star/eccomerce/msvcestadopedidos/MsvcEstadopedidosApplication.java
@@ -2,11 +2,12 @@ package kruger.star.eccomerce.msvcestadopedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
+
 public class MsvcEstadopedidosApplication {
 
 	public static void main(String[] args) {
--- msvc-negocios/src/main/java/kruger/star/eccomerce/msvcnegocios/MsvcNegociosApplication.java
+++ msvc-negocios/src/main/java/kruger/star/eccomerce/msvcnegocios/MsvcNegociosApplication.java
@@ -2,10 +2,11 @@ package kruger.star.eccomerce.msvcnegocios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class MsvcNegociosApplication {
 
 	public static void main(String[] args) {
