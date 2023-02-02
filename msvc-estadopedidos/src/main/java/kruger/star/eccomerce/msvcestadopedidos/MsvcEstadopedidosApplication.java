a0d766c98c93482ebe4e8feb49fd21b06d42202d
Luisito <luisitoylag@gmail.com>
2023-02-02 04:50:31 -0500

    Actualizacion  de dependencias de Eureka  y corrigiendo pom de Productos

--- msvc-estadopedidos/src/main/java/kruger/star/eccomerce/msvcestadopedidos/MsvcEstadopedidosApplication.java
+++ msvc-estadopedidos/src/main/java/kruger/star/eccomerce/msvcestadopedidos/MsvcEstadopedidosApplication.java
@@ -2,10 +2,11 @@ package kruger.star.eccomerce.msvcestadopedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class MsvcEstadopedidosApplication {
 
 	public static void main(String[] args) {
