b0ebf65b6a11db55b8ef2ddf72e69336183ca666
Luisito <luisitoylag@gmail.com>
2023-02-02 06:31:19 -0500

    Configuración del Gateway

---
+++ gateway-service/src/test/java/kruger/eccomerce/star/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kruger.eccomerce.star.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
