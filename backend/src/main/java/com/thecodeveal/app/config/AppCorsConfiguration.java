08c177c06b111836487d6eec9d40ab99bef286a8
Luisito <luisitoylag@gmail.com>
2023-02-12 19:49:50 -0500

    ConfigCors

---
+++ backend/src/main/java/com/thecodeveal/app/config/AppCorsConfiguration.java
@@ -0,0 +1,28 @@
+package com.thecodeveal.app.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+public class AppCorsConfiguration {
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        List<String> origins = new java.util.ArrayList<>();
+        origins.add("http://localhost:3000");
+        config.setAllowedOrigins(origins);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
